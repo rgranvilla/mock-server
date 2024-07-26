8e3f0df433e2e9de154ee5279f678c7e35d8f05c
Ricardo Granvilla <rgranvilla@gmail.com>
2024-07-26 17:09:51 -0300

    explanation of how crud routes are generated

--- api_v1/tools.js
+++ api_v1/tools.js
@@ -1,51 +1,50 @@
 /**
  * Required libraries
  */
-const bcrypt = require('bcrypt')
-const readLine = require('readline')
-const async = require('async')
+import bcrypt from "bcrypt";
+import readLine from "readline";
 
 // Password hash method
-const hashPassword = plain => bcrypt.hashSync(plain, 8)
+const hashPassword = (plain) => bcrypt.hashSync(plain, 8);
 
 // Ask user password method
 function askPassword(question) {
   return new Promise((resolve, reject) => {
     const rl = readLine.createInterface({
       input: process.stdin,
-      output: process.stdout
-    })
+      output: process.stdout,
+    });
 
-    rl.question(question, answer => {
-      rl.close()
-      resolve(answer)
-    })
-  })
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
 }
 
 // Generate hash password method
 async function generateHash() {
   try {
-    console.log('**********************************')
-    console.log('**     Password hash script     **')
-    console.log('**********************************')
+    console.log("**********************************");
+    console.log("**     Password hash script     **");
+    console.log("**********************************");
 
     const passwordAnswer = await askPassword(
-      'Please give me a password to hash: '
-    )
+      "Please give me a password to hash: "
+    );
 
-    if (passwordAnswer != '') {
-      const hashedPassword = hashPassword(passwordAnswer)
-      const compare = bcrypt.compareSync(passwordAnswer, hashedPassword)
-      await console.log('Hashed password:', hashedPassword)
-      await console.log('Valdiation:', compare)
+    if (passwordAnswer != "") {
+      const hashedPassword = hashPassword(passwordAnswer);
+      const compare = bcrypt.compareSync(passwordAnswer, hashedPassword);
+      await console.log("Hashed password:", hashedPassword);
+      await console.log("Valdiation:", compare);
     } else {
-      console.log('You need write something. Script aborted!')
+      console.log("You need write something. Script aborted!");
     }
   } catch (err) {
-    console.log(err)
-    return process.exit(1)
+    console.log(err);
+    return process.exit(1);
   }
 }
 
-generateHash()
+generateHash();
